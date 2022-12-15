0a4e3b824313a332604ca463ee57b03e9e142b70
Fadhilah <fadhilah.larasaty@next-ti.co.id>
2022-12-15 16:33:14 +0700

    Adding footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <HeaderView/>
-
+    <!-- <p>Welcome to our page</p> -->
+    <FooterView msg="Fadhilah Tri Larasaty"/>
   </div>
 </template>
 
 <script>
 
   import HeaderView from './components/HeaderView.vue';
+  import FooterView from './components/FooterView.vue';
 
   export default {
     name: 'App',
     components: {
       HeaderView,
+      FooterView
     },
   }
 </script>
---
+++ src/components/FooterView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="footerview">
+    <!-- <p> Developed by {{msg}} </p> -->
+    <b-navbar style="margin-top:100px" toggleable="lg" type="dark" variant="info">
+        <p>&copy; <a href="https://github.com/Fadhilahlaras">{{msg}}</a>, All Right Reserved.</p>
+    </b-navbar>
+  </div>
+</template>
+
+
+<script>
+
+    export default {
+        name: 'FooterView',
+        props: {
+            msg: String
+        },
+    }
+
+</script>
