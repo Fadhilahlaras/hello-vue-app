7532160489d7a2635cfd63e31306bc8f36d7f872
Fadhilahlaras <fadhilahlaras97@gmail.com>
2023-02-27 17:00:47 +0700

    Modify event listeners

--- src/components/Topic/EventListeners.vue
+++ src/components/Topic/EventListeners.vue
@@ -7,51 +7,34 @@
 
     <b-container>
         <b-row class="mt-4">
-            <b-col cols="4">
-                <b-card
-                    title="Card 1"
-                    img-top-tag="article"
-                    class="mb-2"
-                    >
-                    <img id="show-modal" @click="showModal = true" class="card-img-top" src="../../assets/sky.jpg" alt="Image">
-                    <b-card-text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. </b-card-text>
-                    <b-card-text> Total Like : {{ count }} </b-card-text>
-                </b-card>
-                <button @click="increment"> Like </button>
-                <button @click="decrement" style="margin-left:20px"> Dislike </button>
-                <!-- <button id="show-modal" @click="showModal = true">Show Modal</button> -->
-                <Teleport to="body">
-                    <!-- use the modal component, pass in the prop -->
-                    <modal :show="showModal" @close="showModal = false">
-                    <template #header>
-                        <h3>custom header</h3>
-                    </template>
-                    </modal>
-                </Teleport>
-            </b-col>
-            <b-col cols="4">
-                <b-card
-                    title="Card 2"
-                    img-src="./assets/logo.png"
-                    img-alt="Image"
-                    img-top-tag="article"
-                    class="mb-2"
-                    >
-
-                    <b-card-text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. </b-card-text>
-                </b-card>
-            </b-col>
-            <b-col cols="4">
-                <b-card
-                    title="Card 3"
-                    img-src="./assets/logo.png"
-                    img-alt="Image"
-                    img-top-tag="article"
-                    class="mb-2"
-                    >
-
-                    <b-card-text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. </b-card-text>
-                </b-card>
+            <b-col v-for="card in cards" :key="card.id" cols="4">
+            <b-card
+                :title="`Card ${card.id}`"
+                img-top-tag="article"
+                class="mb-2"
+            >
+                <img
+                :id="`show-modal-${card.id}`"
+                @click="showModal = card.id"
+                class="card-img-top"
+                :src="require(`../../assets/kota/${card.id}.jpg`)"
+                alt="Image"
+                />
+                <b-card-text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. </b-card-text>
+                <b-card-text> Total Like : {{ card.count }} </b-card-text>
+            </b-card>
+            <button @click="increment(card.id)"> Like </button>
+            <button @click="decrement(card.id)" style="margin-left:20px"> Dislike </button>
+            <!-- <Teleport to="body">
+                <modal
+                :show="showModal === card.id"
+                @close="showModal = null"
+                >
+                <template #header>
+                    <h3>custom header</h3>
+                </template>
+                </modal>
+            </Teleport> -->
             </b-col>
         </b-row>
     </b-container>
@@ -62,7 +45,7 @@
 
 <script>
 
-    import Modal from '../Topic/ModalEvent.vue'
+    // import Modal from '../Topic/ModalEvent.vue'
 
     export default {
         name: 'EventListeners',
@@ -70,21 +53,28 @@
             return {
                 titleClass: 'titleEvent',
                 count: 0,
-                showModal: false
+                showModal: false,
+                cards: [
+                    { id: 1, count: 0 },
+                    { id: 2, count: 0 },
+                    { id: 3, count: 0 }
+                ]
             }
         },
-        components: {
-            Modal
-        },
+        // components: {
+        //     Modal
+        // },
     methods: {
-        increment() {
-            this.count++
+        increment(cardId) {
+            const card = this.cards.find(c => c.id === cardId)
+            card.count++
         },
-        decrement() {
-            if(this.count <= 0) {
-                alert("The count already 0, cannot decrement again")
+        decrement(cardId) {
+            const card = this.cards.find(c => c.id === cardId)
+            if(card.count <= 0) {
+                alert("The count already 0")
             } else {
-                this.count--
+                card.count--
             }
         }
     }
